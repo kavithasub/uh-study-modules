5c5aecaa631aa4cf0cc163cc6398939835c91f9c
Kavitha <kthatchu@gmail.com>
2024-01-18 17:32:38 +0000

    Update script for exponential growth

--- applied-data-science/assignment/assignment-3/assignment3.py
+++ applied-data-science/assignment/assignment-3/assignment3.py
@@ -10,6 +10,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import cluster_tools as ct
 import sklearn.cluster as cluster
+import scipy.optimize as opt
 
 
 def read_data(datafile):
@@ -20,16 +21,15 @@ def read_data(datafile):
     return data
 
 
-def mani_data(df, indicators):
-    # Pivot the data for plotting
-    df = pd.DataFrame(data)
-    df_pivot = df.reset_index().groupby(['Country Name', 'Series Name'])[
-        '2010 [YR2010]'].aggregate('first').unstack()
-    #df_pivot = df.pivot(index=df.index, columns='Series Name')['2010 [YR2010]']
-    # df_pivot = df.pivot_table(
-    #   values='2010 [YR2010]', index=df.index, columns='Series Name', aggfunc='first')
-    #df_pivot = df_pivot.dropna()
-    return df_pivot
+def manipulate_data(df_m):
+    df_m['2010'].replace({'..': '0'}, inplace=True)
+    df_m = df_m.dropna()
+
+    df_m = df_m.reset_index().groupby(['Country Name', 'Series Name'])[
+        '2010'].aggregate('first').unstack()
+    df_m = df_m.reset_index(drop=True)
+    
+    return df_m
 
 
 def exponential_growth(t, scale, growth):
@@ -37,15 +37,14 @@ def exponential_growth(t, scale, growth):
     return f
 
 
-def print_to_file(df_m):
+def logistics(t, a, k, t0):
+    """ Computes logistics function with scale and incr as free parameters
     """
-    This method is common print used to print given data to text file
-    """
-    x = np.random.randint(1, 100)
-    filename = "dataframe" + str(x) + ".txt"
-    textfile = open(filename, "w")
-    df_m.to_string(textfile)
-    textfile.close()
+    
+    f = a / (1.0 + np.exp(-k * (t - t0)))
+    
+    return f
+
 
 
 # Read data file
@@ -60,57 +59,33 @@ df = df.rename(columns={'1970 [YR1970]': '1970', '1980 [YR1980]': '1980',
                         '1990 [YR1990]': '1990', '2000 [YR2000]': '2000',
                         '2010 [YR2010]': '2010', '2020 [YR2020]': '2020'})
 
-
 # Manipulate dataframe
 df_m = df.copy()
-#df_new['2010'] = round(df_new['2010'], 3)
-
-df_m['2010'].replace({'..': '0'}, inplace=True)
-#df_m = pd.to_numeric(df_m['2010'])
-
-# print(type(df_m['2010'][0]))
-df_m = df_m.dropna()
-#df_m['2010'] = df_m['2010'].astype(float)
-# print(df_new.head())
-df_m = df_m.reset_index().groupby(['Country Name', 'Series Name'])[
-    '2010'].aggregate('first').unstack()
-
-df_m = df_m.reset_index(drop=True)
-
-print(df_m.head())
+df_m = manipulate_data(df_m)
 
 ##corr = df_m.corr()
 
-# df_clus = df_m[["People using at least basic drinking water services (% of population)",
-#                 "Water productivity, total (constant 2015 US$ GDP per cubic meter of total freshwater withdrawal)"]].copy()
+# Exract two indicators for climate change fitting
+clus_col_1 = 'Annual freshwater withdrawals, agriculture (% of total freshwater withdrawal)'
+clus_col_2 = 'Renewable internal freshwater resources, total (billion cubic meters)'
 
-df_clus = df_m[["Annual freshwater withdrawals, agriculture (% of total freshwater withdrawal)",
-                "Renewable internal freshwater resources, total (billion cubic meters)"]].copy()
-
-
+df_clus = df_m[[clus_col_1, clus_col_2]].copy()
 df_clus = df_clus.astype(float)
-df_clus.to_excel('test3.xlsx')
+# Normalise dataframe
 df_clus2, df_clus_min, df_clus_max = ct.scaler(df_clus)
 
-
+# Plot for 4 clusters
 ncluster = 4
 
-#plt.figure(figsize=(8, 8))
-#cm = plt.colormaps["Paired"]
-x = df_clus["Annual freshwater withdrawals, agriculture (% of total freshwater withdrawal)"]
-y = df_clus["Renewable internal freshwater resources, total (billion cubic meters)"]
-#plt.scatter(x, y, ncluster, marker="d", cmap=cm)
-#plt.xlabel("Using drinking water population %")
-#plt.ylabel("Water productivity")
-#plt.show()
+x = df_clus[clus_col_1]
+y = df_clus[clus_col_2]
 
-
+#set up kmeans and fit
 kmeans = cluster.KMeans(n_clusters=ncluster, n_init=20)
 kmeans.fit(df_clus2)
 
 labels = kmeans.labels_
 cen = kmeans.cluster_centers_
-# print(cen)
 
 plt.figure(figsize=(6.0, 6.0))
 # scatter plot with colours selected using the cluster numbers
@@ -133,20 +108,24 @@ plt.show()
 
 
 
-'''
-
 # fit exponential growth
-popt, pcorr = opt.curve_fit(exponential_growth, df["date"], df["Population"])
+popt, pcorr = opt.curve_fit(exponential_growth, df_m["year"], df_m["freshwater resources"],
+                            p0=[4e8, 0.03])
+# much better
+print("Fit parameter", popt)
 
-print("fit parameter 1990: ", popt[0])
-print("growth rate : ", popt[1])
-df["popExponential"] = exponential_growth(df["date"], *popt)
-
+df_m["res_exp"] = exponential_growth(df["year"], *popt)
 plt.figure()
-plt.plot(df["date"], exponential_growth(
-    df["date"], popt[0], popt[1]), label="trial fit")
-plt.plot(df["date"], df["Population"])
-plt.xlabel("Year")
+plt.plot(df["date"], df["Population"], label="data")
+plt.plot(df["date"], df["pop_exp"], label="fit")
+
 plt.legend()
+plt.title("Final fit exponential growth")
 plt.show()
-'''
+print()
+
+print("Population in")
+print("2030:", exponential_growth(2030, *popt) / 1.0e6, "Mill.")
+print("2040:", exponential_growth(2040, *popt) / 1.0e6, "Mill.")
+print("2050:", exponential_growth(2050, *popt) / 1.0e6, "Mill.")
+
